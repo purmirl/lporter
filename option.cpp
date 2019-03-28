7a5b91d5140330e29af8512bf152ce03bf26e62f
purmirl <gmlrnjs1994@gmail.com>
2019-03-28 16:38:10 +0900

    option.cpp is added.

---
+++ option.cpp
@@ -0,0 +1,12 @@
+/**
+ * lporter project.
+ * Simple IDS for CentOS System.
+ * ------
+ *  Copyright 2019 PeTrA. All rights reserved.
+ */
+
+#include <iostream>
+#include "option.h"
+
+using namespace std;
+
